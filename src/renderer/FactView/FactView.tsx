7b5aadab82fff710aa10442fe5559d60e2414678
Alex Barrett <dev@alexbarrett.com>
2023-11-19 21:28:36 +0000

    Add basic view displaying a cat fact

---
+++ src/renderer/FactView/FactView.tsx
@@ -0,0 +1,14 @@
+import classes from './FactView.module.css';
+
+function FactView() {
+  return (
+    <div className={classes.fact}>
+      <p>
+        When a domestic cat goes after mice, about 1 pounce in 3 results in a
+        catch.
+      </p>
+    </div>
+  );
+}
+
+export default FactView;
