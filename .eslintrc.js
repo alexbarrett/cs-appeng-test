4a6ae73857354e58700182db86619eb65e739f6a
Alex Barrett <dev@alexbarrett.com>
2023-11-20 17:17:21 +0000

    Update ESLint config to allow for...of loops
    The default AirBnB configuration disallows them and the reasoning for
    doing so is dubious. See https://github.com/eslint/eslint/issues/4267
    for more info.

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,29 @@ module.exports = {
     '@typescript-eslint/no-shadow': 'error',
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'error',
-    'react/require-default-props': 'off', // Not necessary as default parameter types can be checked by TypeScript.
+    // Not necessary as default parameter types can be enforced by TypeScript.
+    'react/require-default-props': 'off',
+    // This is the same as the default configuration expect it removes the rule
+    // disallowing 'for...of' statements which has dubious reasoning
+    // (see https://github.com/eslint/eslint/issues/4267)
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message:
+          'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+      },
+      {
+        selector: 'LabeledStatement',
+        message:
+          'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message:
+          '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+    ],
   },
   parserOptions: {
     ecmaVersion: 2022,
